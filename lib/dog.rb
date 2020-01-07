e6c7b062d651623f165b0e37340825592acd5a79
Cody Toney-Griffith <cody.toney@gmail.com>
2020-01-07 02:44:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,15 @@
-fido = dog.new 
-
-fido.name = "Fido"
-fido.name
-
-fido.bark
-wolf
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
+ 
+  def name
+    @this_dogs_name
+  end
+end
+ 
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
